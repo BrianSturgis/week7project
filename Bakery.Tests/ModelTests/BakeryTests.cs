5738c6c42939fa04a28a148bf31b2300c287389f
Brian Sturgis <sturujisu@gmail.com>
2020-12-12 18:30:10 -0800

    this is a good fail for test 1 Pastry_PastryCreation_Pastry

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -1,20 +1,16 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using Bakery.Models;
-
-
-
 namespace Bakery.Tests
 {
   [TestClass]
   public class BakeryTests
   {
     [TestMethod] 
-    public void Bread_BreadCreation_Bread()
+    public void Pastry_PastryCreation_Pastry()
     {
-
-
-      
+      // Pastry newPastry = new Pastry(1);
+      // Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
-  
+}
 }
